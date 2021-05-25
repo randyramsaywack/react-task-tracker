d5718f535c71a536a7f21a181ff7e9608dad8fc7
Randy Ramsaywack <ramsaywackr@gmail.com>
2021-05-25 02:14:23 -0400

    Started Add Popup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -26,13 +27,27 @@ function App() {
 
   // Delete Task
   const deleteTask = (id) => {
-    console.log("delete", id);
+    setTasks(tasks.filter((tasks) => tasks.id !== id));
+  };
+
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
   };
 
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} onDelete={deleteTask} />
+      <AddTask />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No Tasks To Show"
+      )}
     </div>
   );
 }
